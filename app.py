bb6ee39f2a69e2235238789af1c9c047e7b07edd
yashraj1008 <131208085+yashraj1008@users.noreply.github.com>
2023-12-13 21:19:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,10 +65,10 @@ with col2:
     if st.button('Diabetes Test Result'):
         diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
         
-        if (diab_prediction[0] == 1):
-          diab_diagnosis = 'The person is diabetic'
-        else:
+        if (diab_prediction[0] == 0):
           diab_diagnosis = 'The person is not diabetic'
+        else:
+          diab_diagnosis = 'The person is diabetic'
         
     st.success(diab_diagnosis)
 
